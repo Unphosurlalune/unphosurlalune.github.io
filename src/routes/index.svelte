38c0b133866e02e666f742c0a5e6afe9582298fa
Aurélien Gerbore <Deuh@users.noreply.github.com>
2021-11-28 20:10:11 +0000

    Setup sveltekit and initial graphcms integration

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+    import { gql, GraphQLClient } from 'graphql-request'
+  
+    export async function load() {
+      const graphcms = new GraphQLClient(
+        import.meta.env.VITE_GRAPHCMS_URL,
+        {
+          headers: {},
+        }
+      )
+  
+      const query = gql`
+        query PostsIndex {
+          posts {
+            id
+            title
+            slug
+            date
+            excerpt
+          }
+        }
+      `
+  
+      const { posts } = await graphcms.request(query)
+  
+      return {
+        props: {
+          posts,
+        },
+      }
+    }
+  </script>
+  
+  <script>
+    export let posts
+  </script>
+  
+  <h1>UnPhoSurLaLune</h1>
+  <ul>
+    {#each posts as post}
+    <li>
+      <a href="/post/{post.slug}">{post.title}</a>
+    </li>
+    {/each}
+  </ul> 
+
+  <style>
+      :global(body) {
+          background-color: #b7b8ba;
+          color: #383C49;
+          /*
+           color1: #445369;
+            color2: #b7b8ba;
+            color3: #849cac;
+            color4: #847c84;
+            color5: #2c2c2c;
+      }
+  </style>
