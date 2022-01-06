15b2ce4d4395b0023cf45af893b521ce4194c2d3
Aurélien Gerbore <Deuh@users.noreply.github.com>
2022-01-06 19:30:55 +0000

    add flux rss JMT

--- src/lib/externalTile.svelte
+++ src/lib/externalTile.svelte
@@ -3,7 +3,7 @@
     export let url = '#_';
 </script>
 
-<a href="{url}" class="relative inline-block px-4 py-2 font-medium group my-1">
+<a href="{url}" target="_blank" class="relative inline-block px-4 py-2 font-medium group my-1">
     <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-gray-300 group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
     <span class="absolute inset-0 w-full h-full bg-white border-2 border-gray-300 group-hover:bg-gray-300"></span>
     <span class="relative text-black group-hover:text-white">{text}</span>
---
+++ src/routes/rss/index.svelte
@@ -0,0 +1,46 @@
+<script>
+import PageTitle from "$lib/pageTitle.svelte";
+import ExternalTile from '$lib/externalTile.svelte';
+let flux = [
+    '100.xml',
+    '130.xml',
+    '160.xml',
+    '190.xml',
+    '220.xml',
+    '260.xml',
+    '110.xml',
+    '140.xml',
+    '170.xml',
+    '200.xml',
+    '230.xml',
+    '270.xml',
+    '120.xml',
+    '150.xml',
+    '180.xml',
+    '210.xml',
+    '240.xml',
+    'paraboles.xml'
+];
+
+</script>
+
+<PageTitle title='Des liens Rss' />
+Sources:
+<ExternalTile url="https://www.enseignements-bibliques-jmthobois.com" text="www.enseignements-bibliques-jmthobois.com"/>
+<ExternalTile url="https://www.youtube.com/channel/UCqikEBiVFFPmPhj0CtBoJrQ" text="Youtube: Association Keren"/>
+<br>
+<br>
+Flux:
+<ul class="">
+    {#each flux as flu}
+    <li>
+        <ExternalTile text="{flu}" url="rss/jmt/{flu}" />
+    </li>
+    {/each}
+</ul>
+
+<style>
+    p {
+        margin-bottom: 10px;
+    }
+</style>
