666ab378c3642c3a5ebd7747a7e0e21708ca6bcf
Aurélien Gerbore <Deuh@users.noreply.github.com>
2022-01-07 18:52:21 +0000

    add post in categories and change font size

--- src/routes/categories/[slug].svelte
+++ src/routes/categories/[slug].svelte
@@ -1,46 +1,63 @@
 <script context="module">
-    import { gql, GraphQLClient } from 'graphql-request';
-    import PageTitle from '$lib/pageTitle.svelte';
-  
-    export async function load(context) {
-      const graphcms = new GraphQLClient(
-        import.meta.env.VITE_GRAPHCMS_URL,
-        {
-          headers: {},
-        }
-      )
-  
-      const query = gql`
-        query getCategory($slug: String!) {
-          category(where: { slug: $slug }) {
-            name,
-            description
-          }
-        }
-      `
-  
-      const variables = {
-        slug: context.page.params.slug,
-      }
-      const { category } = await graphcms.request(query, variables)
-  
-      return {
-        props: {
-          category,
-        },
-      }
-    }
-  </script>
-  
-  <script>
-    export let category
-  </script>
-  
-  <svelte:head>
-    <title>{category.name}</title>
-  </svelte:head>
-  
-  <div class="text-center w-full">
-  <PageTitle title='{category.name}' />
-  <p>{category?.description}</p>
-  </div> 
+	import { gql, GraphQLClient } from 'graphql-request';
+	import PageTitle from '$lib/pageTitle.svelte';
+	import BlogTile from '$lib/BlogTile/index.svelte';
+
+	export async function load(context) {
+		const graphcms = new GraphQLClient(import.meta.env.VITE_GRAPHCMS_URL, {
+			headers: {}
+		});
+
+		const query = gql`
+			query getCategory($slug: String!) {
+				category(where: { slug: $slug }) {
+					name
+					description
+					posts {
+						... on Post {
+							id
+							title
+							slug
+							date
+							excerpt
+							author {
+								name
+							}
+						}
+					}
+				}
+			}
+		`;
+
+		const variables = {
+			slug: context.page.params.slug
+		};
+		const { category } = await graphcms.request(query, variables);
+
+		return {
+			props: {
+				category
+			}
+		};
+	}
+</script>
+
+<script>
+	export let category;
+</script>
+
+<svelte:head>
+	<title>{category.name}</title>
+</svelte:head>
+
+<div class="text-center w-full">
+	<PageTitle title={category.name} />
+	<p>{category?.description}</p>
+	<ul>
+		{#each category?.posts as post}
+			<li>
+				<BlogTile {post} />
+			</li>
+		{/each}
+	</ul>
+</div>
